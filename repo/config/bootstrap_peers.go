bdb88b8ae5d6cb146ac07184a157b0c7ae183c80
scroot <whsse@msn.com>
2017-07-12 15:08:35 +0800

    + 新的自举节点

--- repo/config/bootstrap_peers.go
+++ repo/config/bootstrap_peers.go
@@ -14,8 +14,9 @@ import (
 // NOTE: This is here -- and not inside cmd/ipfs/init.go -- because of an
 // import dependency issue. TODO: move this into a config/default/ package.
 var DefaultBootstrapAddresses = []string{
-	"/ip4/221.234.44.17/tcp/9800/ipfs/QmPWDPN8KAybm9AV1xMiYJR17MUd33pNgtHET7SAU7Wkzx",              //dl.morequick.net
-        "/ip4/112.74.197.10/tcp/10001/ipfs/QmPU2xTqv4MuDYrbJTEsnYYGR4ChDaVBVGBUh4yrnwEZ3x",             //aliyun
+	"/ip4/61.184.173.13/tcp/4001/ipfs/QmSraCsx5vMGxMcKTnLrY92AVaF2DLijphDGZADpdxCYum",             //xianling
+	"/ip4/221.234.44.17/tcp/9800/ipfs/QmPWDPN8KAybm9AV1xMiYJR17MUd33pNgtHET7SAU7Wkzx",             //dl.morequick.net
+	"/ip4/112.74.197.10/tcp/10001/ipfs/QmPU2xTqv4MuDYrbJTEsnYYGR4ChDaVBVGBUh4yrnwEZ3x",            //aliyun
 	"/ip4/104.131.131.82/tcp/4001/ipfs/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",            // mars.i.ipfs.io
 	"/ip4/104.236.176.52/tcp/4001/ipfs/QmSoLnSGccFuZQJzRadHn95W2CrSFmZuTdDWP8HXaHca9z",            // neptune.i.ipfs.io
 	"/ip4/104.236.179.241/tcp/4001/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM",           // pluto.i.ipfs.io
